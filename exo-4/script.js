c9a070d0e2361298b5449357958814bf2984f917
vozoul <vozoul@gmail.com>
2019-04-02 13:12:54 +0200

    end forth

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,3 +3,52 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+
+function rmLast(){
+    secretMessage.pop();
+}
+
+function toProgram(){
+    secretMessage.push('to');
+    secretMessage.push('programme');
+}
+
+function modify(input, output){
+    secretMessage.forEach(function(item, index, array){
+       if(item === input){
+           return secretMessage[index] = output;
+       }
+    });
+}
+
+function delFirst(){
+    secretMessage.shift();
+}
+
+function addProg(value){
+    secretMessage.unshift(value);
+}
+
+function uKnow(){
+    secretMessage.forEach(function(item, index){
+        if(item === 'get' || item === 'right' || item === 'the' || item === 'first' || item === 'time'){
+            if(item === 'get'){
+                secretMessage[index] = 'know';
+            }else{
+                secretMessage.splice(index, 1);
+            }
+        }
+    });
+}
+
+function launch(){
+    rmLast();
+    toProgram();
+    modify('easily', 'right');
+    delFirst();
+    addProg('Programming');
+    uKnow();
+    console.log(secretMessage.join(' '));
+}
+
+launch();
