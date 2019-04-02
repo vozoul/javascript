639737cf64c5e5873439f4db880500051bcf043c
vozoul <vozoul@gmail.com>
2019-04-02 14:26:20 +0200

    end fifth

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -3,9 +3,26 @@
 console.log('exo-5');
 
 var input = prompt('Mot ou Texte a traduire en baleine ...');
-
 var voyels = ['a','e','i','o','u','y'];
-
 var resArray = [];
 
-var resultArray = input.split('');
+function chante(){
+    var inputArray = input.split('');
+    // for(var i = 0; i < input.length; i++){
+    //     for(var v = 0; v < voyels.length; v++){
+    //         if(inputArray[i] == voyels[v]){
+    //             resArray.push(inputArray[i]);
+    //         }
+    //     }
+    // }
+
+    inputArray.forEach(function(item){
+        if(voyels.indexOf(item) != -1){
+            resArray.push(item);
+        }
+    });
+    window.alert(resArray.join('').toUpperCase());
+    // réponse 5.8 : on affiche le resultat d'un tableau pour palier a ceci on utilise la fonction .join();
+}
+
+chante();
