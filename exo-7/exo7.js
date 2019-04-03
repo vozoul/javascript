f0887e6bc4c65b70dab4c8e176e76843f1771d7e
vozoul <vozoul@gmail.com>
2019-04-03 17:06:20 +0200

    seventh in cours

--- exo-7/exo7.js
+++ exo-7/exo7.js
@@ -1,3 +1,25 @@
 console.log("exo-7");
 
 console.log(jsonDatas);
+
+var typeValue = {
+    car: "voiture",
+    house: "maison",
+    game: "jeu",
+    videoGame: "jeu video",
+    show: "spectacle",
+}
+
+console.log();
+
+function trad(source){
+    source.forEach(function(item){
+       var key = item.type;
+       var keys = Object.keys(typeValue);
+       if(keys.indexOf(key) != -1){
+           item.type = typeValue[key];
+       }
+    });
+}
+
+trad(jsonDatas);
