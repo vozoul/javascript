65262bfc14c6a8a37256d55150e4c41439bd5b11
vozoul <vozoul@gmail.com>
2019-04-04 16:57:47 +0200

    7.8

--- exo-7/exo7.js
+++ exo-7/exo7.js
@@ -2,27 +2,27 @@ console.log("exo-7");
 
 console.log(jsonDatas);
 
-function trad(source){
-    source.forEach(function(item){
-        item.type=translate(item.type);
+function trad(source) {
+    source.forEach(function (item) {
+        item.type = translate(item.type);
     });
 }
 
-function translate(key){
+function translate(key) {
     var keys = Object.keys(typeValue);
-    if(keys.indexOf(key) != -1){
+    if (keys.indexOf(key) != -1) {
         return typeValue[key];
     }
 }
 
-function trouve(){
+function trouve() {
     var resultat = [];
     console.clear();
     document.getElementById('liste').innerHTML = '';
     var checked = document.getElementById('checked').checked;
     var recherche = document.getElementById('search').value;
-    jsonDatas.forEach(function(item){
-        if(item.type === recherche || item.type === translate(recherche)) {
+    jsonDatas.forEach(function (item) {
+        if (item.type === recherche || item.type === translate(recherche)) {
             if (!checked && item.quantity > 0) {
                 console.log(item);
                 resultat.push(item);
@@ -40,35 +40,34 @@ function trouve(){
 }
 
 
-
-function filtering(resultat){
+function filtering(resultat) {
     document.getElementById('liste').innerHTML = "";
     var select = document.getElementById('filter').value;
     console.log(select);
-    if(select === '1'){
-        resultat.sort(function(a,b){
-           return (a.name).localeCompare(b.name);
+    if (select === '1') {
+        resultat.sort(function (a, b) {
+            return (a.name).localeCompare(b.name);
         });
-    }else if (select === '2'){
-        resultat.sort(function(a,b){
+    } else if (select === '2') {
+        resultat.sort(function (a, b) {
             return (b.name).localeCompare(a.name);
         });
-    }else if(select === '3') {
-        resultat.sort(function(a,b){
-            return (a.price)-(b.price);
+    } else if (select === '3') {
+        resultat.sort(function (a, b) {
+            return (a.price) - (b.price);
         });
-    }else if(select === '4') {
-        resultat.sort(function(a,b){
-            return (b.price)-(a.price);
+    } else if (select === '4') {
+        resultat.sort(function (a, b) {
+            return (b.price) - (a.price);
         });
     }
-    resultat.forEach(function(item){
+    resultat.forEach(function (item) {
         document.getElementById('liste').innerHTML += "<li>" + item.name + "</li>";
     })
     console.log(resultat);
 }
 
-function addProduct(){
+function addProduct() {
     jsonDatas.push({
             name: document.getElementById('newName').value,
             type: document.getElementById('newType').value,
