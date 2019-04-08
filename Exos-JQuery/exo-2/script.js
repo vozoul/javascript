d97b419eec756eb995a97a2e6e26b4cc4154fa47
vozoul <vozoul@gmail.com>
2019-04-08 12:13:39 +0200

    jQuery 2 finished

--- Exos-JQuery/exo-2/script.js
+++ Exos-JQuery/exo-2/script.js
@@ -1,2 +1,22 @@
 console.log('exercice 2');
+/*
+principaux selecteurs:
+Tag
+id
+class
+ */
 
+$(document).ready(function(){
+    console.log(
+        $("#title"),
+        $(".sidenav"),
+        $("footer"),
+        $(".input-group"),
+        $(".glyphicon"),
+        $(".label-success"),
+        $(".article"),
+        $(".glyphicon-time"),
+        $(".sidenav li:first-of-type"),
+        $(".sidenav:odd"),
+    );
+});
