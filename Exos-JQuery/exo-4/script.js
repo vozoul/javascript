3f3d5e5f606fa2c6312d3a8112f0b3d5c359962e
vozoul <vozoul@gmail.com>
2019-04-09 12:39:34 +0200

    jQuery 4 finished

--- Exos-JQuery/exo-4/script.js
+++ Exos-JQuery/exo-4/script.js
@@ -1,6 +1,50 @@
 console.log("exercice 4");
 
+$(document).ready(function(){
+    $('form button.btn.btn-danger').on('click', function(){
+        var $mail = $('footer form input:first-of-type').val();
+        var $mailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})$/;
+        if($mail === ''){
+            window.alert('Veuillez renseigner votre mail');
+        }else if(!$mailReg.test($mail)){
+            window.alert("l'adresse : " + $mail + ", n'est pas valide. vérifiez le contenu");
+        }else{
+            console.log($mail);
+            window.alert('Merci nous vous tiendrons informé des différentes offres sur l\'adresse ' + $mail);
+        }
+    });
 
-$('form button.btn.btn-danger').click(function(){
-    window.alert('Merci nous vous tiendrons informé des différentes offres');
+    $("li a:contains('Products')").on('dblclick', function(){
+        $(this).hide();
+    });
+
+    var $count = 1;
+    var $icone = $('a:contains("Cart") span')[0];
+
+    $('img').on('click', function(){
+
+        var $valCart = parseInt($('a:contains("Cart")').text().replace(/[^\d]/g, ""));
+        var $valeur = parseInt($(this).closest('.panel').children('.panel-footer').text().replace(/[^\d]/g, ""));
+        if($valCart > 0){
+            var $nbrCart = $valeur + $valCart;
+        }else{
+            var $nbrCart = $valeur;
+        }
+        console.log($nbrCart);
+
+        $('a:contains("Cart")').text(' Cart ' + $nbrCart);
+        $('a:contains("Cart")').prepend($icone);
+    })
+
+    // $('a, img, h1, input, button').on('mouseover', function(event){
+    $('.panel').on('mouseover', function(){
+        console.log('l\'utilisateur regarde : ' + $(this).children('.panel-footer').text());
+    })
+
+    $('form input').on('keydown', function(){
+        console.log($('form input').val());
+    })
+
+
 });
+
