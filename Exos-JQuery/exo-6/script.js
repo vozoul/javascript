d2133136dbeaa0af1db5603ba301a18619a3e92c
vozoul <vozoul@gmail.com>
2019-04-10 11:37:51 +0200

    jQuery 6 finished

--- Exos-JQuery/exo-6/script.js
+++ Exos-JQuery/exo-6/script.js
@@ -1,8 +1,26 @@
 console.log("exercice 6");
 
 $(document).ready(function(){
+
     $('.alert').hide();
+
     $('#connection').on('click', function(){
-       window.alert('Merci');
+        var $valeur = ['verifiez le(s) champ(s) '];
+        var $error = 0;
+        $('input').each(function (){
+            if($(this).val() === ''){
+                $valeur.push($(this).attr('name'));
+                $error++;
+            }
+        });
+
+        //regexs
+        var $mailValide = (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,5})+$/.test($('#email').val())) ? '' : $error++ + $valeur.push('email invalide');
+        var $passValide = (/^.{6,}$/.test($('#password').val())) ? '' : $error++ + $valeur.push('mot de passe trop court');
+
+        var sortie = ($error === 0 ) ?
+            $('.alert').hide() + $('.alert.alert-success').toggle().fadeIn().text('vous êtes connecter') :
+            $('.alert').hide() + $('.alert.alert-danger').toggle().fadeIn().text($valeur)
+        ;
     });
 });
