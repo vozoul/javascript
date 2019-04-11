4a85f7c62513465bbce58d5e46128c6e26320cac
vozoul <vozoul@gmail.com>
2019-04-11 22:38:14 +0200

    add form

--- Exos-JQuery/exo-7/script.js
+++ Exos-JQuery/exo-7/script.js
@@ -1,72 +1,69 @@
 console.log("exercice 7");
 
-function getData() {
-    return data; // data is defined in DATA.js file
-}
-
 var $trItem = [];
+var $addedItems = [];
+var $totalDatas = $($addedItems).empty() ? $trItem : $trItem + $addedItems;
 var $lastSortBy = '';
+var $source = $($trItem).empty() ? data : $totalDatas;
 
 $(document).ready(function () {
 
+    $('table').show();
+    $('#register').hide();
+
     //je visionne la BDD
     $('#showDb').on('click', function () {
+        $('.table').show();
+        $('.pagination').show();
+        $('#showForm').removeClass('active');
+        $('#register').hide();
+            $(this).addClass('active');
         $('tbody').html('');
-        $(getData()).each(function (index, item) {
+        $($source).each(function (index, item) {
             addLine(item);
         });
+        console.log(lastId())
+        pagin();
+    });
+
+    //affichage formulaire
+    $('#showForm').on('click',function(){
+        $('.table').hide()
+        $('.pagination').hide()
+        $('#showDb').removeClass('active');
+        $('#register').show()
+            $(this).addClass('active');
     });
 
     //lancement du tri
     $('tr a.sort').on('click', function(){
         var $sortBy = $(this).data('sort');
         sorting($sortBy);
-    })
+        pagin();
+    });
 
     //je recherche dans la BDD
     $('#search').on('click', function () {
         seek();
+        pagin();
     })
 
     //Pagination
     $('.maxRows').on('click', function () {
-        $('.pagination').html('');
-        var maxRows = parseInt($(this).data('value'));
-        var numLine = 0;
-        var totalRows = $('tbody tr').length;
-        $('tbody tr').each(function () {
-            numLine++;
-            if (numLine > maxRows) {
-                $(this).hide();
-            }
-            if (numLine < maxRows) {
-                $(this).show();
-            }
-        })
-        if (totalRows > maxRows) {
-            var numPage = Math.ceil(totalRows / maxRows)
-            for (var i = 1; i <= numPage;) {
-                $('.pagination').append('<li class="page-item" data-page="' + i + '"><a href="#" class="page-link">' + i++ + '</a></li>').show()
-            }
-        }
-        $('.pagination li:first-child').addClass('active');
-        $('.pagination li').on('click', function () {
-            var numPage = $(this).data('page');
-            var lineIndex = 0;
-            $('.pagination li').removeClass('active')
-            $('tbody tr').each(function () {
-                lineIndex++;
-                if (lineIndex > (maxRows * numPage) || lineIndex < ((maxRows * numPage) - maxRows)) {
-                    $(this).hide();
-                } else {
-                    $(this).show();
-                }
-            })
-        })
+        $('.maxRows').removeClass('active');
+        $(this).addClass('active');
+        $maxRows = parseInt($(this).data('value'));
+        pagin();
     });
 
 });
 
+//lastId
+function lastId(){
+    base = parseInt($trItem.slice(-1)[0].id);
+    added = (!$($addedItems).empty()) ? parseInt($addedItems.slice(-1)[0].id) : 0 ;
+    return base + added;
+}
 
 //fonction d'ajout de ligne
 function addLine(item) {
@@ -144,4 +141,42 @@ function sorting(sortBy){
         addLine(item)
     });
     if($sens === true){$sens = false}else{$sens = true};
+}
+
+//pagination
+var $maxRows;
+function pagin(){
+    $('.pagination').html('');
+    maxRows = $maxRows;
+    var numLine = 0;
+    var totalRows = $('tbody tr').length;
+    $('tbody tr').each(function () {
+        numLine++;
+        if (numLine > maxRows) {
+            $(this).hide();
+        }
+        if (numLine < maxRows) {
+            $(this).show();
+        }
+    })
+    if (totalRows > maxRows) {
+        var numPage = Math.ceil(totalRows / maxRows)
+        for (var i = 1; i <= numPage;) {
+            $('.pagination').append('<li class="page-item" data-page="' + i + '"><a href="#" class="page-link">' + i++ + '</a></li>').show()
+        }
+    }
+    $('.pagination li:first-child').addClass('active');
+    $('.pagination li').on('click', function () {
+        var numPage = $(this).data('page');
+        var lineIndex = 0;
+        $('.pagination li').removeClass('active')
+        $('tbody tr').each(function () {
+            lineIndex++;
+            if (lineIndex > (maxRows * numPage) || lineIndex < ((maxRows * numPage) - maxRows)) {
+                $(this).hide();
+            } else {
+                $(this).show();
+            }
+        })
+    })
 }
